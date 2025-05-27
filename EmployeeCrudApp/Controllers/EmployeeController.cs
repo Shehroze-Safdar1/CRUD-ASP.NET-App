eb07d60fc5d65a72fc1297bf9398dd84b8ab0ff5
Shehroze-Safdar1 <shehroze.safdar@gmail.com>
2025-05-27 14:40:30 +0500

    Complete Project

---
+++ EmployeeCrudApp/Controllers/EmployeeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using EmployeeCrudApp.Models;
+
+namespace EmployeeCrudApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeDbEntities db = new EmployeeDbEntities();
+
+        public ActionResult Index()
+        {
+            var employees = db.Employees.ToList();
+            return View(employees);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var employee = db.Employees.Find(id);
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var employee = db.Employees.Find(id);
+            return View(employee);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
